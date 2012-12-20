0607f55a1bb42a614a7888e5a06f36024957b0dc
joe <joe@ced48e53-b730-46bd-8c2e-541a19ddac72>
2012-12-20 12:44:29 +0000

    rc1
    git-svn-id: https://ligodv.areeda.com/svn/ligodv-1-14@854 ced48e53-b730-46bd-8c2e-541a19ddac72

--- branches/plotGUI-devel/ligodv/main/ldvparams.m
+++ branches/plotGUI-devel/ligodv/main/ldvparams.m
@@ -8,7 +8,7 @@
 % current version number used for update check NB: 1.13b1 < 1.13 (the
 % released version).
 global curVersion;
-curVersion = '1.14b2';
+curVersion = '1.14rc1';
 
 % Where to report problems
 global contact;

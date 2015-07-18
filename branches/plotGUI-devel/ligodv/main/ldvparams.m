31ea45c551d2b6f9a0deb5ad94dda63caac5d48f
joe <joe@ced48e53-b730-46bd-8c2e-541a19ddac72>
2015-07-18 22:31:47 +0000

    ldvparams.m: version bump
    git-svn-id: https://ligodv.areeda.com/svn/ligodv-1-14@2567 ced48e53-b730-46bd-8c2e-541a19ddac72

--- branches/plotGUI-devel/ligodv/main/ldvparams.m
+++ branches/plotGUI-devel/ligodv/main/ldvparams.m
@@ -8,7 +8,7 @@
 % current version number used for update check NB: 1.13b1 < 1.13 (the
 % released version).
 global curVersion;
-curVersion = '1.15b2';
+curVersion = '1.16b2';
 
 % Where to report problems
 global contact;
